d7d39d18345ab14e10de19401137d33bbdc95ef2
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2022-12-13 17:14:49 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Car car = new Car();
+        Car secondCar = new Car();
+        car.color = "Blue";
+        secondCar.color = "green";
+
+
+        System.out.println(car);
+        System.out.println(secondCar);
+
+        System.out.println(car.color);
+
+        Car thirdCar = new Car();
+        System.out.println(thirdCar);
+        System.out.println(thirdCar.number);
+        System.out.println(thirdCar.isSupercar);
+        System.out.println(thirdCar.color);
+    }
+
+}
